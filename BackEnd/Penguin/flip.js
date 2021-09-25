799476ddf402835d73058ffd848eb2a641b4d7db
Vatsal Nath <vatsal@knights.ucf.edu>
2021-09-24 21:31:17 -0400

    penguin
    penguin test files

---
+++ BackEnd/Penguin/flip.js
@@ -0,0 +1,82 @@
+import { useEffect, useState, useRef } from "react";
+import Card from "./card";
+import uniqueElementsArray from './data';
+import "./app.scss";
+
+export default function App() {
+  const [cards, setCards] = useState(
+    () => shuffleCards(uniqueCardsArray.concat(uniqueCardsArray))
+  );
+  const [openCards, setOpenCards] = useState([]);
+  const [clearedCards, setClearedCards] = useState({});
+  const [moves, setMoves] = useState(0);
+  const [showModal, setShowModal] = useState(false);
+  const timeout = useRef(null);
+
+  // Check if both the cards have same type. If they do, mark them inactive
+  const evaluate = () => {
+    const [first, second] = openCards;
+    if (cards[first].type === cards[second].type) {
+      setClearedCards((prev) => ({ ...prev, [cards[first].type]: true }));
+      setOpenCards([]);
+      return;
+    }
+    // Flip cards after a 500ms duration
+    timeout.current = setTimeout(() => {
+      setOpenCards([]);
+    }, 500);
+  };
+  
+  const handleCardClick = (index) => {
+    // Have a maximum of 2 items in array at once.
+    if (openCards.length === 1) {
+      setOpenCards((prev) => [...prev, index]);
+      // increase the moves once we opened a pair
+      setMoves((moves) => moves + 1);
+    } else {
+      // If two cards are already open, we cancel timeout set for flipping cards back
+      clearTimeout(timeout.current);
+      setOpenCards([index]);
+    }
+  };
+
+  useEffect(() => {
+    if (openCards.length === 2) {
+      setTimeout(evaluate, 500);
+    }
+  }, [openCards]);
+
+  const checkIsFlipped = (index) => {
+    return openCards.includes(index);
+  };
+
+  const checkIsInactive = (card) => {
+    return Boolean(clearedCards[card.type]);
+  };
+
+  return (
+    <div className="App">
+      <header>
+        <h3>Play the Flip card game</h3>
+        <div>
+          Select two cards with same content consequtively to make them vanish
+        </div>
+      </header>
+      <div className="container">
+        {cards.map((card, index) => {
+          return (
+            <Card
+              key={index}
+              card={card}
+              index={index}
+              isDisabled={shouldDisableAllCards}
+              isInactive={checkIsInactive(card)}
+              isFlipped={checkIsFlipped(index)}
+              onClick={handleCardClick}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
